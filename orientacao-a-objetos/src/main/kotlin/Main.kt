e87e8dc5100b915b737becdf66913db48903e5d7
Thais Sousa <sousatcm@gmail.com>
2022-04-05 11:11:35 -0300

    Finalizando aula 02 - Controle de fluxo e o paradigma procedural

--- orientacao-a-objetos/src/main/kotlin/Main.kt
+++ orientacao-a-objetos/src/main/kotlin/Main.kt
@@ -1,13 +1,64 @@
 fun main(){
     println("Bem vinde ao ByteBank!")
 
-    val titular = "Thais"
-    val numeroConta = 1000
-    var saldoConta = 0.0
-    saldoConta = 100 + 2.0
-    saldoConta += 200
+    loop@ for (i in 1..100) {
+        println("i $i")
+        for (j in 1..100) {
+            println("j $j")
+            if (j == 5) break@loop
+        }
+    }
+    
 
-    println("Titular da Conta: $titular")
-    println("Número da Conta: $numeroConta")
-    println("Saldo Atual da Conta: $saldoConta")
+//    var i = 0
+//    while(i < 5){
+//        val titular = "Thais $i"
+//        val numeroConta = 1000 + i
+//        var saldoConta = i + 0.0
+//        i++
+//
+//        println("Titular da Conta: $titular")
+//        println("Número da Conta: $numeroConta")
+//        println("Saldo Atual da Conta: $saldoConta")
+//        println()
+//    }
+
+
+//    for(i in 5 downTo 1){
+//
+//        if(i == 4) {
+//            continue
+//        }
+//
+//        val titular = "Thais $i"
+//        val numeroConta = 1000 + i
+//        var saldoConta = i + 0.0
+//
+//
+//        println("Titular da Conta: $titular")
+//        println("Número da Conta: $numeroConta")
+//        println("Saldo Atual da Conta: $saldoConta")
+//        println()
+//
+//        testaCondicoes(saldoConta)
+//    }
 }
+
+
+fun testaCondicoes(saldoConta: Double){
+//    if (saldoConta > 0.0) {
+//        println("A conta é positiva!")
+//    } else if (saldoConta == 0.0) {
+//        println("A conta é neutra!")
+//    } else {
+//        println("A conta é negativa!")
+//    }
+
+    when {
+        saldoConta > 0.0 -> println("A conta é positiva!")
+        saldoConta == 0.0 -> println("A conta é neutra!")
+        else -> println("A conta é negativa!")
+    }
+}
+
+
