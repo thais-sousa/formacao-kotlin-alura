3d0987ea94f61775c311029f40e01feadbac2869
Thais Sousa <sousatcm@gmail.com>
2022-04-25 09:33:02 -0300

    Adicionando aula 03 - Variáveis globais e objects

--- pacotes-e-composicao/src/main/kotlin/br/com/thais/bytebank/main.kt
+++ pacotes-e-composicao/src/main/kotlin/br/com/thais/bytebank/main.kt
@@ -1,7 +1,29 @@
 package teste
 
+import br.com.thais.bytebank.modelo.Client
+import br.com.thais.bytebank.modelo.Conta
+import br.com.thais.bytebank.modelo.ContaCorrente
+import br.com.thais.bytebank.modelo.ContaPoupanca
 fun main() {
+
+    val monteiro = object {
+        val nome: String = "Monteiro"
+        val cpf: String = ""
+        val senha: Int = 2
+
+        fun autentica(senha: Int) = this.senha == senha
+    }
+
+    println("Nome do cliente ${monteiro.nome}")
+
+    val thais = Client(nome = "Thais", cpf = "", senha = 1)
+
+    val contaPoupanca = ContaPoupanca(thais, 1001)
+    val contaCorrente = ContaCorrente(thais, 1001)
+
     testaContas()
+
+    println("Total de Contas: ${Conta.total}")
 }
 
 
--- pacotes-e-composicao/src/main/kotlin/br/com/thais/bytebank/modelo/Conta.kt
+++ pacotes-e-composicao/src/main/kotlin/br/com/thais/bytebank/modelo/Conta.kt
@@ -1,5 +1,4 @@
 package br.com.thais.bytebank.modelo
-
 abstract class Conta(
     var titular: Client,
     val numero: Int
@@ -7,10 +6,46 @@ abstract class Conta(
     var saldo = 0.0
         protected set
 
+    companion object Contador {
+        var total = 0
+            private set
+    }
     fun deposita(valor: Double) {
         if (valor > 0) {
             saldo += valor
         }
     }
+
     abstract fun saca(valor: Double)
+}
+
+class ContaCorrente(
+    titular: Client,
+    numero: Int
+) : ContaTransferivel(
+    titular = titular,
+    numero = numero
+) {
+    override fun saca(valor: Double) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor
+        }
+    }
+
+}
+
+class ContaPoupanca(
+    titular: Client,
+    numero: Int
+) : ContaTransferivel(
+    titular = titular,
+    numero = numero
+) {
+
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        if (this.saldo >= valorComTaxa) {
+            this.saldo -= valorComTaxa
+        }
+    }
 }
