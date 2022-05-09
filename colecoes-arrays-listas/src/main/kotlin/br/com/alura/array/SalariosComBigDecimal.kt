11ebf2c143797da1486cea32cabbbe664c2ec490
Thais Sousa <sousatcm@gmail.com>
2022-05-09 17:40:04 -0300

    Utilizando reduce, filde e compondo operações

--- colecoes-arrays-listas/src/main/kotlin/br/com/alura/array/SalariosComBigDecimal.kt
+++ colecoes-arrays-listas/src/main/kotlin/br/com/alura/array/SalariosComBigDecimal.kt
@@ -9,19 +9,48 @@ fun main() {
     val aumento = "1.1".toBigDecimal()
     var aumentoMinimo = "500.00".toBigDecimal()
     val salariosComAumento = salarios.map { salario ->
-        if (salario < "5000.00".toBigDecimal()) {
-            salario + aumentoMinimo
-        } else {
-            (salario * aumento).setScale(2, RoundingMode.UP)
-        }
+        calculaAumentoRelativo(salario, aumentoMinimo, aumento)
     }.toTypedArray()
 
     println(salarios.contentToString())
     println(salariosComAumento.contentToString())
+
+
+    val gastoInicial = salariosComAumento.somatoria()
+    println("Gasto com salários no primeiro mês: $gastoInicial")
+
+
+    val meses = 6.toBigDecimal()
+    val gastoTotal = salariosComAumento.fold(gastoInicial) { acc, salario ->
+        acc + (salario * meses).setScale(2, RoundingMode.UP)
+    }
+
+    println("Gasto com salários em seis meses: $gastoTotal")
+
+
+    val mediaMaioresSalarios = salariosComAumento
+        .sorted()
+        .takeLast(3)
+        .toTypedArray()
+        .media()
+
+    println("Média dos três maiores salários: $mediaMaioresSalarios")
+
+    val mediaMenoresSalarios = salariosComAumento
+        .sorted()
+        .take(3)
+        .toTypedArray()
+        .media()
+
+    println("Média dos três menores salários: $mediaMenoresSalarios")
+}
+private fun calculaAumentoRelativo(
+    salario: BigDecimal,
+    aumentoMinimo: BigDecimal,
+    aumento: BigDecimal
+) = if (salario < "5000.00".toBigDecimal()) {
+    salario + aumentoMinimo
+} else {
+    (salario * aumento).setScale(2, RoundingMode.UP)
 }
 
-fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
-    return Array<BigDecimal>(valores.size) { i ->
-        valores[i].toBigDecimal()
-    }
-}
