1dcedf99fafb53af229c9d508d56c7edb4bb22c4
Thais Sousa <sousatcm@gmail.com>
2022-05-12 14:49:11 -0300

    Testando o distinct

--- collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
+++ collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
@@ -1,8 +1,13 @@
 package br.com.alura
-
 fun main() {
-    testaCopia()
+    val assistiramCursoAndroid: List<String> = listOf<String>("Thais", "Marco", "Logan", "Dimitri")
+    val assistiramCursoKotlin: List<String> = listOf("Thais", "Vitória", "João")
+    val assistiramAmbos: List<String> = assistiramCursoAndroid + assistiramCursoKotlin
 
+//    assistiramAmbos.addAll(assistiramCursoAndroid)
+//    assistiramAmbos.addAll(assistiramCursoKotlin)
+
+    println(assistiramAmbos.distinct())
 }
 
 
