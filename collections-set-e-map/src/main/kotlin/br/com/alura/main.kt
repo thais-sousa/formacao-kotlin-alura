64e9f74e8279993cd1d04c876bd8e400bd87a5cd
Thais Sousa <sousatcm@gmail.com>
2022-05-16 11:51:41 -0300

    Testando o Map

--- collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
+++ collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
@@ -1,7 +1,18 @@
 package br.com.alura
 
 fun main() {
+//  mapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    val pedidos = mapOf(1 to 20.0, 2 to 10.0, 3 to 25.0)
+    println(pedidos)
 
+    val pedido = pedidos[1]
+    pedido?.let {
+        println("Pedido ${it}")
+    }
+
+    for(pedido in pedidos) {
+        println("Número do pedido: ${pedido.key} - Valor do pedido: $ ${pedido.value}")
+    }
 }
 
 
