44fbe98ffeea7532a51063b46c92e402ef7d53e5
Thais Sousa <sousatcm@gmail.com>
2022-05-16 11:13:56 -0300

    Organizando arquivos

--- collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
+++ collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
@@ -1,23 +1,8 @@
 package br.com.alura
 
 fun main() {
-    val assistiramCursoAndroid: Set<String> = setOf("Thais", "Marco", "Logan", "Dimitri")
-    val assistiramCursoKotlin: Set<String> = setOf("Thais", "Vitória", "João")
-    //    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
-    val assistiramAmbos: MutableSet<String> = mutableSetOf()
-    assistiramAmbos.addAll(assistiramCursoAndroid)
-    assistiramAmbos.addAll(assistiramCursoKotlin)
-    assistiramAmbos.add("Walle")
-    println(assistiramAmbos)
 
-    println(assistiramCursoAndroid union assistiramCursoKotlin)
-    println(assistiramCursoAndroid subtract assistiramCursoKotlin)
-    println(assistiramCursoAndroid intersect assistiramCursoKotlin)
-
-    val assistiramList: MutableList<String> = assistiramAmbos.toMutableList()
-    assistiramList.add("Thais")
-    println("assistiramList: ${assistiramList}")
-    println("assistiramList - Set: ${assistiramList.toSet()}")
 }
+
 
 
