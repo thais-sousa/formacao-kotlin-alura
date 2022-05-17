cb1443dac8f5b752720b3e20b1b8ca836a6d5250
Thais Sousa <sousatcm@gmail.com>
2022-05-16 23:36:26 -0300

    Utilizando a associação

--- collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
+++ collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
@@ -1,9 +1,34 @@
 package br.com.alura
+
 fun main() {
 
+    val pedidos = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 60.0),
+        Pedido(3, 30.0),
+        Pedido(4, 70.0)
+    )
+
+    println(pedidos)
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido ->
+        pedido.numero
+    }
+
+    println(pedidosMapeados)
+    println(pedidosMapeados[1])
+
+    //associateWith
+
+    val pedidosFreteGratis = pedidos.associateWith { pedido ->
+        pedido.valor > 50.0
+    }
+
+    println(pedidosFreteGratis)
+    println(pedidosFreteGratis[Pedido(1, 20.0)])
 
 }
 
+data class Pedido(val numero: Int, val valor: Double)
 
 
 
