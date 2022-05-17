6be49a4d6051295144eccc6c37933687a5ca9617
Thais Sousa <sousatcm@gmail.com>
2022-05-17 00:01:48 -0300

    Utilizando o agrupamento

--- collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
+++ collections-set-e-map/src/main/kotlin/br/com/alura/main.kt
@@ -26,6 +26,35 @@ fun main() {
     println(pedidosFreteGratis)
     println(pedidosFreteGratis[Pedido(1, 20.0)])
 
+//    val mapa: Map<Boolean, Pedido> = pedidos.associateBy { pedido ->
+//        pedido.valor > 50.0
+//    }
+//    println(mapa)
+
+    val pedidosFreteGratisAgrupados: Map<Boolean, List<Pedido>> =
+        pedidos.groupBy { pedido: Pedido ->
+            pedido.valor > 50.0
+        }
+
+    println(pedidosFreteGratisAgrupados)
+    println(pedidosFreteGratisAgrupados[true])
+
+    val nomes = listOf<String>(
+        "Thais",
+        "Marco",
+        "Maria",
+        "Logan",
+        "Vitória",
+        "Valentina",
+        "Walle",
+        "Thiago"
+    )
+
+    val agenda: Map<Char, List<String>> = nomes.groupBy { nome ->
+        nome.first()
+    }
+    println(agenda)
+    println(agenda['T'])
 }
 
 data class Pedido(val numero: Int, val valor: Double)
